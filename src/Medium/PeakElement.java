adbc972128fb3c80707edc9065a40295d5f214ca
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-03-25 02:07:55 -0700

    "Solution for Leetcode problem 162, peak element in a mountain array."

---
+++ src/Medium/PeakElement.java
@@ -0,0 +1,18 @@
+package Medium;
+// LC 162, find peak element. Same code as peak index.
+public class PeakElement {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
