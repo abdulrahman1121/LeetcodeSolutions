054c48685e501fafadff27bccd74ed8baeb705ab
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-07-07 23:54:28 -0700

    Solution for Leetcode problem 92, reverse a Linked List.

---
+++ src/Medium/ReverseLL.java
@@ -0,0 +1,43 @@
+package Medium;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLL {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode cur = prev.next;
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = cur.next;
+            cur.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+
+        return dummy.next;
+    }
+}
+
